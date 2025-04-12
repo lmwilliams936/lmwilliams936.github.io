f0afb9a0c5622e8bc5d6cb96b4cf9a93fb13c652
lmwilliams936 <lmwilliams27@my.waketech.edu>
2025-04-11 20:21:42 -0400

    .

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+function populateGradebook(data) {
+    console.log("populating gradebook with data", data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
